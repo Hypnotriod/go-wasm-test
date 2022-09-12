3f20e5a45b33c4e0ae1e6dd49ff33aa93a979641
ipikin <illia.pikin@dgngames.com>
2022-09-12 19:10:49 +0300

    initial

---
+++ cmd/wasm/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"syscall/js"
+	"time"
+)
+
+func main() {
+	js.Global().Set("goWasmTest", js.FuncOf(goWasmTest))
+	fmt.Println("Test from Go WASM")
+	<-make(chan struct{})
+}
+
+func goWasmTest(this js.Value, args []js.Value) interface{} {
+	if len(args) == 1 && args[0].Type() == js.TypeFunction {
+		go func() {
+			time.Sleep(1 * time.Second)
+			args[0].Invoke("Message from goroutine delayed by a second")
+		}()
+		return js.Undefined()
+	}
+	return errors.New("argument is not a function")
+}
