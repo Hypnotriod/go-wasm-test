d0b1ac966cb40fddcb07a4472a7a45066b933f54
ipikin <illia.pikin@dgngames.com>
2022-09-16 18:34:53 +0300

    Playing aroun with tinygo

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # go-wasm-test
 Go Web Assembly playground
+
+
+## Links:
+* [tinygo](https://tinygo.org/getting-started/install/)
+* [binaryen](https://github.com/WebAssembly/binaryen/releases)
--- cmd/http/main.go
+++ cmd/http/main.go
@@ -3,13 +3,19 @@ package main
 import (
 	"log"
 	"net/http"
+	"strings"
 )
 
+const directory = "./public"
+
 func main() {
-	fs := http.FileServer(http.Dir("./public"))
-	http.Handle("/", fs)
-	err := http.ListenAndServe(":3000", nil)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fs := http.FileServer(http.Dir(directory))
+	log.Print("Serving " + directory + " on http://localhost:3000")
+	http.ListenAndServe(":3000", http.HandlerFunc(func(resp http.ResponseWriter, req *http.Request) {
+		resp.Header().Add("Cache-Control", "no-cache")
+		if strings.HasSuffix(req.URL.Path, ".wasm") {
+			resp.Header().Set("content-type", "application/wasm")
+		}
+		fs.ServeHTTP(resp, req)
+	}))
 }
